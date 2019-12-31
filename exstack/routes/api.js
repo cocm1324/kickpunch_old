4a7e0076add1eccf86f925bbec758dee9e45ba67
cocm1324 <cocm1324@gmail.com>
2020-01-01 00:38:22 +0900

    end of this year

--- exstack/routes/api.js
+++ exstack/routes/api.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const mongoose = require('mongoose');
-const db = 'mongodb+srv://<username>:<password>@dev-kwvql.gcp.mongodb.net/<dbname>';
+const db = 'mongodb+srv://kickpunch:firePro17!@dev-kwvql.gcp.mongodb.net/kickpunch';
 
+const User = require('../models/user');
+
+// connect to mongodb
 mongoose.connect(db, { 
     useNewUrlParser: true,
     useUnifiedTopology: true 
@@ -18,6 +21,15 @@ router.get('/', (req, res) => {
     res.send('From API route');
 });
 
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
 
+    // save user data to mongo db -> use 'save' method
+    user.save((error, registeredUser) => {
+        if(error) console.log(error);
+        else res.status(200).send(registeredUser);
+    });
+});
 
 module.exports = router;
